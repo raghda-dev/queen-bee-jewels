abad23a92622a0f724d71f6a39d15f1d8bb6033a
raghda-dev <80541073+raghda-dev@users.noreply.github.com>
2026-04-28 09:55:07 +0300

    Add production URL to CORS configuration

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://queen-bee-jewels.onrender.com'],
   credentials: true,
 }));
 
